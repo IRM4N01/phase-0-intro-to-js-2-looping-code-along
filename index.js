252a576cf5bc8671f72d60311ebeaf3c8f784669
Irman Wibawa <irman.w@hotmail.com>
2025-07-25 15:51:09 +1000

    looping code along completed, all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Irman", "Maeve", "Leia"];
+const event = "Birthday party";
+
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+    return messages
+}
+
+
+function countDown() {
+    for (let countDown = 10; countDown >= 0; countDown--) {
+        console.log(countDown);
+    }
+}
+
+countDown();

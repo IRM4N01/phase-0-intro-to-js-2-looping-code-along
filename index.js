3fed4b5696fa5e49de45dd331b69eb30e80aae8e
Irman Wibawa <irman.w@hotmail.com>
2025-11-01 21:33:03 +1100

    looping code along completed, all tests passed

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 // Code your solutions in this file
-const names = ["Irman", "Maeve", "Leia"];
-const event = "Birthday party";
+const names = []
+const event = ""
+
 
 function writeCards(names, event) {
   const messages = [];
 
   for (let i = 0; i < names.length; i++) {
-    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-    messages.push(message);
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    messages.push(message)
+    debugger;
   }
-    return messages
+
+  return messages;
 }
 
+console.log(writeCards(["Mako", "Iroh", "Goku"], "Graduation"));
 
 function countDown() {
-    for (let countDown = 10; countDown >= 0; countDown--) {
-        console.log(countDown);
-    }
+  for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown);
+  }
 }
 
 countDown();
